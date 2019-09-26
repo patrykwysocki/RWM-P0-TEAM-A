2d493dc451995b9a54a8ec36be5a17a70f8d6652
patryk wysocki <c00213855@itcarlow.ie>
2019-09-26 11:23:22 +0100

    test of running total and decrement if less than together successful

---
+++ Assets/Scripts/CombinedFilter.cs
@@ -0,0 +1,9 @@
+﻿public class CombinedFilter
+{
+
+    public static int[] combinedFilter(int[] xs)
+    {
+        return RunningTotalFilter.RunningTotals(DecrementIfLessThanFirst.answerList(xs));
+    }
+
+}
---
+++ Assets/Tests/CombinedFilterTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class CombinedFilterTest
+    {
+        [Test]
+        public void CombinedFilterSimple()
+        {
+            //Assert.IsTrue(false);
+
+            int[] input = { 4,2,8 };
+            int[] output = CombinedFilter.combinedFilter(input);
+            int[] expected = { 4,5,13};
+
+            //foreach (var o in output)
+            //{
+            //    Debug.Log(o);
+            //}
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
